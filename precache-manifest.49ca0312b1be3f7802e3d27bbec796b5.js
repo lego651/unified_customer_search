b6697899333a0d83310825308b65bb1aaff00ad2
Jason Gao <jason@Jasons-MacBook-Pro.local>
2019-03-15 03:42:13 -0700

    Updates

---
+++ precache-manifest.49ca0312b1be3f7802e3d27bbec796b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c070b8d94e0e0174f754",
+    "url": "/unified_customer_search/static/css/main.7918d58e.chunk.css"
+  },
+  {
+    "revision": "c070b8d94e0e0174f754",
+    "url": "/unified_customer_search/static/js/main.84acd8a3.chunk.js"
+  },
+  {
+    "revision": "d1ce1ace7baefed67ec2",
+    "url": "/unified_customer_search/static/js/runtime~main.0815355e.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/unified_customer_search/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unified_customer_search/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "274f4f3543ab674578aead638b105342",
+    "url": "/unified_customer_search/index.html"
+  }
+];
